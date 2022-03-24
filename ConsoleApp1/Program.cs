f3bc0dc56c0ca9b3065f3aaae4e80f9552e31b47
yakir barzilai <notcooldudet@gmail.com>
2022-03-24 15:41:50 +0200

    hi

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int []grades = new int[5];
+            int i;
+            Console.WriteLine("please enter" + grades.Length + "grades");
+            for (i = 0; i < grades.Length; i++) 
+            grades[i] = int.Parse(Console.ReadLine());
+
+            for (i = 0; i < grades.Length; i++)
+            Console.Write(grades[i] + " ");
+        }
+    }
+}
